d1a4e9e659443ac40bc86f42f04ce0f808e1e13e
Nemanja Djuric <ne@nemanja.io>
2020-05-11 18:25:14 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-time php bin/magento maintenance:enable;
-time /usr/local/bin/php -dmemory_limit=20000M bin/magento setup:upgrade;
-time /usr/local/bin/php -dmemory_limit=20000M bin/magento setup:di:compile;
-time /usr/local/bin/php -dmemory_limit=20000M bin/magento setup:static-content:deploy --jobs=$(nproc);
+php bin/magento maintenance:enable;
+/usr/local/bin/php -dmemory_limit=20000M bin/magento setup:upgrade;
+/usr/local/bin/php -dmemory_limit=20000M bin/magento setup:di:compile;
+/usr/local/bin/php -dmemory_limit=20000M bin/magento setup:static-content:deploy --jobs=$(nproc);
 /usr/share/stratus/cli autoscaling.reinit;
 sleep 150s;
 echo "\e[41m****Flushing Magento, Varnish, Redis and CloudFront CDN cache at this stage****";
-time /usr/local/bin/php -dmemory_limit=20000M bin/magento cache:clean;
-time /usr/local/bin/php -dmemory_limit=20000M bin/magento cache:flush;
-time /usr/share/stratus/cli cache.cloudfront.invalidate
+/usr/local/bin/php -dmemory_limit=20000M bin/magento cache:clean;
+/usr/local/bin/php -dmemory_limit=20000M bin/magento cache:flush;
+/usr/share/stratus/cli cache.cloudfront.invalidate
 /usr/share/stratus/cli cache.varnish.clear;
 redis-cli -h redis flushall && redis-cli -h redis-config-cache -p 6381 flushall;
-time php bin/magento maintenance:disable;
+php bin/magento maintenance:disable;
 echo "\e[41m****Deployment Finished Site Enabled and tested****";
 status_code=$(curl -kI --header 'Host: cbi2cs52sas1djs9.mojostratus.io' --write-out %{http_code} --silent --output /dev/null 'https://nginx/')
 if [[ "$status_code" -ne 200 ]] ; then
